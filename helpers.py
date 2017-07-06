3361eea905319cc00ff0b08dedb671891f73577a
Alexander Barnes <abarnes023@gmail.com>
2017-07-06 14:52:00 +0000

    Initial commit

---
+++ helpers.py
@@ -0,0 +1,11 @@
+from flask import redirect, request, session, url_for
+from functools import wraps
+
+# requires a user to be logged in
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect(url_for("login", next=request.url))
+        return f(*args, **kwargs)
+    return decorated_function
