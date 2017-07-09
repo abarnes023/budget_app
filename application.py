8e530f79eab9872ebed916aca3bc5f1ecb79831d
Alexander Barnes <abarnes023@gmail.com>
2017-07-09 19:37:09 +0000

    Add register route

--- application.py
+++ application.py
@@ -26,21 +26,51 @@ Session(app)
 # configure to use SQLite database
 db = SQL("sqlite:///budget.db")
 
-# home page
-@app.route("/")
-@login_required
-def index():
-    
-    # TODO
-    
-    return render_template("index.html")
-
 # landing page
 @app.route("/landing")
 def landing():
     """ Direct site visitor to landing page. """
     return render_template("landing.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """ Register a new user. """
     
+    # if user reached via POST
+    if request.method == "POST":
+        
+        # ensure unique username entered
+        if not request.form.get("username"):
+            flash("Please enter a username.", "error")
+            return redirect(url_for("register"))
+        duplicates = db.execute("SELECT * FROM 'users' WHERE username = :username", username=request.form.get("username"))
+        if len(duplicates) > 0:
+            flash("Username is already taken.", "error")
+            return redirect(url_for("register"))
+        
+        # ensure passwords entered match
+        if not request.form.get("password") or not request.form.get("password2"):
+            flash("Please enter and re-enter a password.", "error")
+            return redirect(url_for("register"))
+        if request.form.get("password") != request.form.get("password2"):
+            flash("Passwords don't match.", "error")
+            return redirect(url_for("register"))
+            
+        # insert user into database
+        user_id = db.execute("INSERT INTO 'users' (username, hash) VALUES (:username, :hash)", username=request.form.get("username"), hash=pwd_context.encrypt(request.form.get("password")))
+        
+        # store user id
+        session["user_id"] = user_id
+        
+        # redirect to home page
+        flash("You have successfully been registered.", "message")
+        return redirect(url_for("index"))
+        
+    # if reached route via GET
+    else:
+        return render_template("register.html")
+
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
     """ Log in the user. """
@@ -54,7 +84,7 @@ def login():
         # ensure credentials entered
         if not request.form.get("username"):
             flash("Please enter a username.", "error")
-            redirect(url_for("login"))
+            return redirect(url_for("login"))
         elif not request.form.get("password"):
             flash("Please enter a password.", "error")
             return redirect(url_for("login"))
@@ -76,3 +106,12 @@ def login():
     # if reached via GET
     else:
         return render_template("login.html")
+        
+# home page
+@app.route("/")
+@login_required
+def index():
+    
+    # TODO
+    
+    return render_template("index.html")
