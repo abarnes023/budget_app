b22f7fa49c7216c801a4693d1f64e140a30f6e4d
Alexander Barnes <abarnes023@gmail.com>
2017-07-21 18:38:59 +0000

    Move helper functions to helpers.py

--- helpers.py
+++ helpers.py
@@ -1,5 +1,10 @@
+from cs50 import SQL
 from flask import redirect, request, session, url_for
 from functools import wraps
+from openerp import models, fields, api
+
+# configure to use SQLite database
+db = SQL("sqlite:///budget.db")
 
 # requires a user to be logged in
 def login_required(f):
@@ -9,3 +14,59 @@ def login_required(f):
             return redirect(url_for("landing", next=request.url))
         return f(*args, **kwargs)
     return decorated_function
+
+# check database to see if data for selected month exists    
+def budget_data(month):
+    data = db.execute("SELECT * FROM 'budgets' WHERE user = :user AND month = :month", user=session["user_id"], month=month)
+    if len(data) > 0:
+            # pull values from database
+            e_income = data[0]["e_income"]
+            a_income = data[0]["a_income"]
+            e_rent = data[0]["e_rent"]
+            a_rent = data[0]["a_rent"]
+            e_util = data[0]["e_util"]
+            a_util = data[0]["a_util"]
+            e_food = data[0]["e_food"]
+            a_food = data[0]["a_food"]
+            e_ent = data[0]["e_ent"]
+            a_ent = data[0]["a_ent"]
+            e_save = data[0]["e_save"]
+            a_save = data[0]["a_save"]
+            
+    else:
+            # set variables to None
+            e_income = None
+            a_income = None
+            e_rent = None
+            a_rent = None
+            e_util = None
+            a_util = None
+            e_food = None
+            a_food = None
+            e_ent = None
+            a_ent = None
+            e_save = None
+            a_save = None
+            
+    return {'e_income': e_income, 'a_income': a_income, 'e_rent': e_rent, 'a_rent': a_rent, 'e_util': e_util, 'a_util': a_util, 'e_food': e_food, 'a_food': a_food, 'e_ent': e_ent, 'a_ent': a_ent, 'e_save': e_save, 'a_save': a_save}
+
+def month_change(self,cr,uid,ids,month,context=None):
+    # get month budget data and set currMonth
+    data = budget_data(month)
+    
+    return {
+        'value': {
+            'e_income': data['e_income'],
+            'a_income': data['a_income'],
+            'e_rent': data['e_rent'],
+            'a_rent': data['a_rent'],
+            'e_util': data['e_util'],
+            'a_util': data['a_util'],
+            'e_food': data['e_food'],
+            'a_food': data['a_food'],
+            'e_ent': data['e_ent'],
+            'a_ent': data['a_ent'],
+            'e_save': data['e_save'],
+            'a_save': data['a_save']
+                }
+            }
