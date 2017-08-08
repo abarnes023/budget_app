677824892b41afbb563f5773bcda5eeb1734e3b3
Alexander Barnes <abarnes023@gmail.com>
2017-08-08 17:58:35 +0000

    Add onchange function to responsively change smiles (not working)

--- static/scripts.js
+++ static/scripts.js
@@ -23,17 +23,51 @@ $(function() {
     });
 });
 
-// Call smiles function when new month data loaded
+// Set variables
+var a_income = document.getElementById("a_income");
+var a_rent = document.getElementById("a_rent").value;
+var a_save = document.getElementById("a_save").value;
+var rent = document.getElementById("rent");
+var saved = document.getElementById("saved");
+
 // Call smiles function when income, rent, or savings values changed
+$("#a_income, #a_rent, #a_save").onchange = function smileChange() {
+    smiles(a_rent, a_income, rent);
+    smiles(a_save, a_income, saved);
+};
 
 
-
-// Calculate the percent an expense is of the income
-function smiles(expense, income) {
-    if(expense == null || income == null) {
-        return -1;
+// Calculate the percent an expense is of the income and update smile
+function smiles(expense, income, smile) {
+    // ensure budget values are not null
+    if (expense == null || income == null) {
+        $(smile).innerHTML = '<span style="color:gray;">Enter actual income and rent</span>';
     }
-    else {
-        return (parseFloat(expense))/ (parseFloat(income)) * 100;
+    
+    // calculate spending percentage
+    var percent = (parseFloat(expense))/ (parseFloat(income)) * 100;
+    
+    // set smiles based on spending percent
+    if (smile == rent) {
+        if (percent >= 0 && percent < 25) {
+            $(smile).innerHTML = '<span style="color:green; font-size: 20px">&#9786;</span><span style="color:green;"> You\'re looking great!</span>';
+        }
+        else if (percent >= 25 && percent < 35) {
+            $(smile).innerHTML = '<span style="color:#e1e509; font-size: 20px">&#9863;</span><span style="color:#e1e509;"> Rent spending is okay</span>';
+        }
+        else if (percent >= 35) {
+            $(smile).innerHTML = '<span style="color:red; font-size: 20px">&#9785;</span><span style="color:red;"> Spending too much on rent</span>';
+        }
+    }
+    else if (smile == saved) {
+        if (percent >= 0 && percent < 10) {
+            $(smile).innerHTML = '<span style="color:red; font-size: 20px">&#9785;</span><span style="color:red;"> Cut back and save more</span>';
+        }
+        else if (percent >= 10 && percent < 20) {
+            $(smile).innerHTML = '<span style="color:#e1e509; font-size: 20px">&#9863;</span><spanstyle="color:#e1e509;"> Consider saving a bit more</span>';
+        }
+        else if (percent >= 20) {
+            $(smile).innerHTML = '<span style="color:green; font-size: 20px">&#9786;</span><span style="color:green;"> Great job saving!</span>';
+        }
     }
 }
