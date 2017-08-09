ab337c0de8ba34f2eeee42bd98c2c0b57648f3b8
Alexander Barnes <abarnes023@gmail.com>
2017-08-09 16:26:32 +0000

    Add date validation

--- static/scripts.js
+++ static/scripts.js
@@ -3,27 +3,44 @@
 
 $(function() {
     $('a#set_month').bind('click', function() {
-        $.getJSON(Flask.url_for("month"), {
-            month: $('input[name="month"]').val()
-        }, function(data) {
-            document.getElementById("monthTitle").innerHTML = document.getElementById("month").value,
-            document.getElementById("e_income").value = data.e_income,
-            document.getElementById("a_income").value = data.a_income,
-            document.getElementById("e_rent").value = data.e_rent,
-            document.getElementById("a_rent").value = data.a_rent,
-            document.getElementById("e_util").value = data.e_util,
-            document.getElementById("a_util").value = data.a_util,
-            document.getElementById("e_food").value = data.e_food,
-            document.getElementById("a_food").value = data.a_food,
-            document.getElementById("e_ent").value = data.e_ent,
-            document.getElementById("a_ent").value = data.a_ent,
-            document.getElementById("e_save").value = data.e_save,
-            document.getElementById("a_save").value = data.a_save;
-            smiles(data.a_rent, data.a_income, "rent");
-            smiles(data.a_save, data.a_income, "saved");
-        });
+        if (validMonth('month')) {
+            $.getJSON(Flask.url_for("month"), {
+                month: $('input[name="month"]').val()
+            }, function(data) {
+                document.getElementById("monthTitle").innerHTML = document.getElementById("month").value,
+                document.getElementById("e_income").value = data.e_income,
+                document.getElementById("a_income").value = data.a_income,
+                document.getElementById("e_rent").value = data.e_rent,
+                document.getElementById("a_rent").value = data.a_rent,
+                document.getElementById("e_util").value = data.e_util,
+                document.getElementById("a_util").value = data.a_util,
+                document.getElementById("e_food").value = data.e_food,
+                document.getElementById("a_food").value = data.a_food,
+                document.getElementById("e_ent").value = data.e_ent,
+                document.getElementById("a_ent").value = data.a_ent,
+                document.getElementById("e_save").value = data.e_save,
+                document.getElementById("a_save").value = data.a_save;
+                smiles(data.a_rent, data.a_income, "rent");
+                smiles(data.a_save, data.a_income, "saved");
+            });
+        }
+        else {
+            document.getElementById("monthError").innerHTML = "Please enter a valid month in the format YYYY-MM";
+        }
     });
 });
+
+// Check if month is valid (within 2010-2029)
+
+function validMonth(month) {
+    var monthVal = document.getElementById(month).value;
+    var filter = new RegExp("([2][0][1-2][0-9])([-])(0[123456789]|10|11|12)");
+    if (filter.test(monthVal))
+        return true;
+    else
+        return false;
+}
+
 
 // Call smileChange function when income, rent, or savings values changed
 
