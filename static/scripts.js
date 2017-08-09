c030141e22718fe4f90783c9cdb25035cb3f2fe0
Alexander Barnes <abarnes023@gmail.com>
2017-08-09 16:51:53 +0000

    Set month on change rather than on click, remove set month button

--- static/scripts.js
+++ static/scripts.js
@@ -1,38 +1,38 @@
 
 // Update budget values when the month is changed
-
-$(function() {
-    $('a#set_month').bind('click', function() {
-        if (validMonth('month')) {
-            $.getJSON(Flask.url_for("month"), {
-                month: $('input[name="month"]').val()
-            }, function(data) {
-                document.getElementById("monthTitle").innerHTML = document.getElementById("month").value,
-                document.getElementById("e_income").value = data.e_income,
-                document.getElementById("a_income").value = data.a_income,
-                document.getElementById("e_rent").value = data.e_rent,
-                document.getElementById("a_rent").value = data.a_rent,
-                document.getElementById("e_util").value = data.e_util,
-                document.getElementById("a_util").value = data.a_util,
-                document.getElementById("e_food").value = data.e_food,
-                document.getElementById("a_food").value = data.a_food,
-                document.getElementById("e_ent").value = data.e_ent,
-                document.getElementById("a_ent").value = data.a_ent,
-                document.getElementById("e_save").value = data.e_save,
-                document.getElementById("a_save").value = data.a_save;
-                smiles(data.a_rent, data.a_income, "rent");
-                smiles(data.a_save, data.a_income, "saved");
-            });
-        }
-        else {
-            document.getElementById("monthError").innerHTML = "Please enter a valid month in the format YYYY-MM between 2010-01 to 2029-12";
-        }
-    });
-});
-
 // Disable save budget button on budget.html page if date invalid
 
+$('a#set_month').onchange = setMonth();
 
+function setMonth() {
+    if (validMonth('month')) {
+        document.getElementById("save").disabled = false;
+        $("#monthError").empty();
+        $.getJSON(Flask.url_for("month"), {
+            month: $('input[name="month"]').val()
+        }, function(data) {
+            document.getElementById("monthTitle").innerHTML = document.getElementById("month").value,
+            document.getElementById("e_income").value = data.e_income,
+            document.getElementById("a_income").value = data.a_income,
+            document.getElementById("e_rent").value = data.e_rent,
+            document.getElementById("a_rent").value = data.a_rent,
+            document.getElementById("e_util").value = data.e_util,
+            document.getElementById("a_util").value = data.a_util,
+            document.getElementById("e_food").value = data.e_food,
+            document.getElementById("a_food").value = data.a_food,
+            document.getElementById("e_ent").value = data.e_ent,
+            document.getElementById("a_ent").value = data.a_ent,
+            document.getElementById("e_save").value = data.e_save,
+            document.getElementById("a_save").value = data.a_save;
+            smiles(data.a_rent, data.a_income, "rent");
+            smiles(data.a_save, data.a_income, "saved");
+        });
+    }
+    else {
+        document.getElementById("monthError").innerHTML = "Please enter a valid month in the format YYYY-MM between 2010-01 to 2029-12";
+        document.getElementById("save").disabled = true;
+    }
+}
 
 // Check if month is valid (within 2010-2029)
 
